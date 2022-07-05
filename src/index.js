cc632b824445983abcb4a10b68d99f9d24c0b708
Danny F <accdev101121@acc.edu.au>
2022-07-06 08:45:28 +1000

    Updated cacher

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
  * @author Danny Falero
  * @copyright Copyright 2021 Christian Education Ministries, all rights reserved.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -13,43 +13,54 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * https://raw.githubusercontent.com/acctech/kingjames.bible/master/kjv-src/kjv-1769.txt
  */
 
-import fs from 'fs';
-import Path from 'path';
+import fs from "fs";
+import Path from "path";
 
-export default function Cacher(defaultFolder){
-    const VERBOSE = true;
-    if(!fs.existsSync(defaultFolder)){
-        fs.mkdirSync(defaultFolder);
-    }
-    const folder = defaultFolder;
-    return {
-        save: function(filename, object){
-            if(VERBOSE){
-                console.log("Saving", filename);
-            }
-            fs.writeFileSync(Path.join(folder, filename + ".json"), JSON.stringify(object, null, 2), {encoding:'utf8',flag:'w'});
-        },
-        /**
-         * Load JSON file. Filename does not need path. Filename should be only the file name without extension.
-         * @param filename
-         * @returns {any}
-         */
-        load: function(filename){
-            let filePath = Path.join(folder, filename + ".json");
-            if(fs.existsSync(filePath)){
-                return JSON.parse((fs.readFileSync(filePath, {encoding:'utf8'})).toString());
-            } else {
-                return null;
-            }
-        },
-        exists: function(filename){
-            let filePath = Path.join(folder, filename + ".json");
-            return fs.existsSync(filePath);
-        }
-
-    }
+export default function Cacher(defaultFolder, isVerbose = false) {
+  let VERBOSE = isVerbose;
+  if (!fs.existsSync(defaultFolder)) {
+    fs.mkdirSync(defaultFolder);
+  }
+  const folder = defaultFolder;
+  return {
+    save: function (filename, object) {
+      if (VERBOSE) {
+        console.log("Saving", filename);
+      }
+      fs.writeFileSync(
+        Path.join(folder, filename + ".json"),
+        JSON.stringify(object, null, 2),
+        { encoding: "utf8", flag: "w" }
+      );
+      if (VERBOSE) {
+        console.log("Saved", filename);
+      }
+    },
+    /**
+     * Load JSON file. Filename does not need path. Filename should be only the file name without extension.
+     * @param filename
+     * @returns {any}
+     */
+    load: function (filename) {
+      let filePath = Path.join(folder, filename + ".json");
+      if (fs.existsSync(filePath)) {
+        return JSON.parse(
+          fs.readFileSync(filePath, { encoding: "utf8" }).toString()
+        );
+      } else {
+        return null;
+      }
+    },
+    exists: function (filename) {
+      let filePath = Path.join(folder, filename + ".json");
+      return fs.existsSync(filePath);
+    },
+    listDirectory: function () {
+      return fs.readdirSync(folder);
+    },
+  };
 }
