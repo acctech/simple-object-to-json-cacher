7179b26badc8873f953c279c1d1f5a9875cab269
Danny Falero <52477574+acctech@users.noreply.github.com>
2022-09-15 13:25:35 +1000

    Created tests for all functions

--- __tests__/cacher.spec.js
+++ __tests__/cacher.spec.js
@@ -1,48 +1,92 @@
-import Cacher from '../src/index.js';
-import fs from 'fs';
+const Cacher = require("../dist/index.js");
+const fs = require("fs");
+
+const folderName = "testfolder";
+const testContentObject = [
+  { id: 1, name: "Billy" },
+  { id: 2, name: "Mason" },
+  { id: 3, name: "Todd" },
+];
+
+function deleteAssetsFolder() {
+  if (fs.existsSync(folderName)) {
+    fs.rmSync(folderName, { recursive: true });
+  }
+}
+
+// beforeEach(() => {
+//   deleteAssetsFolder();
+// fs.rmSync("./" + folderName + "/testObject.json");
+// });
 
 describe("Save Cache function", () => {
-    test("It should save a file into folder", () => {
-        const testContentObject = [
-            { id: 1, name: "Billy" },
-            { id: 2, name: "Mason" },
-            { id: 3, name: "Todd" }
-          ];
-      
-          let cacher = Cacher("./testfolder");
-          cacher.save("testObject", testContentObject);
+  let testObjectName = "testObject";
 
-          expect(fs.existsSync("./testfolder/testObject.json")).toEqual(true);
-          fs.rmSync("./testfolder/testObject.json");
-    });
-      test("It should save the given object into the file", () => {
-        const testContentObject = [
-            { id: 1, name: "Billy" },
-            { id: 2, name: "Mason" },
-            { id: 3, name: "Todd" }
-          ];
-      
-          let cacher = Cacher("./testfolder");
-          cacher.save("testObject", testContentObject);
+  test("It should save a file into folder", () => {
+    let cacher = Cacher("./" + folderName);
+    cacher.save(testObjectName, testContentObject);
 
-          expect(JSON.stringify(JSON.parse(fs.readFileSync("./testfolder/testObject.json", "utf8")))).toEqual(JSON.stringify(testContentObject));
-          fs.rmSync("./testfolder/testObject.json");
-      });
+    expect(
+      fs.existsSync("./" + folderName + "/" + testObjectName + ".json")
+    ).toEqual(true);
+    fs.rmSync("./" + folderName + "/" + testObjectName + ".json");
+  });
+  test("It should save the given object into the file", () => {
+    let cacher = Cacher("./" + folderName);
+    cacher.save(testObjectName, testContentObject);
+
+    expect(
+      JSON.stringify(
+        JSON.parse(
+          fs.readFileSync(
+            "./" + folderName + "/" + testObjectName + ".json",
+            "utf8"
+          )
+        )
+      )
+    ).toEqual(JSON.stringify(testContentObject));
+    fs.rmSync("./" + folderName + "/" + testObjectName + ".json");
+  });
 });
 
 describe("Load Cache function", () => {
-    test("It should load the same saved given object", () => {
-        const testContentObject = [
-            { id: 1, name: "Billy" },
-            { id: 2, name: "Mason" },
-            { id: 3, name: "Todd" }
-          ];
-      
-          let cacher = Cacher("./testfolder");
-          cacher.save("testObject", testContentObject);
-          let loadedTestObject = cacher.load("testObject");
+  const testObjectName = "testObject1";
 
-          expect(JSON.stringify(loadedTestObject)).toEqual(JSON.stringify(testContentObject));
-          fs.rmSync("./testfolder/testObject.json");
-      });
+  test("It should load the same saved given object", () => {
+    let cacher = Cacher("./" + folderName);
+    cacher.save(testObjectName, testContentObject);
+    let loadedTestObject = cacher.load(testObjectName);
+
+    expect(JSON.stringify(loadedTestObject)).toEqual(
+      JSON.stringify(testContentObject)
+    );
+    fs.rmSync("./" + folderName + "/" + testObjectName + ".json");
+  });
+});
+
+describe("Exists Cache Function", () => {
+  const testObjectName = "testObject2";
+
+  let cacher = Cacher("./" + folderName);
+  cacher.save(testObjectName, testContentObject);
+  test("Check if Exists cached objects", () => {
+    expect(cacher.exists(testObjectName)).toEqual(true);
+    fs.rmSync("./" + folderName + "/" + testObjectName + ".json");
+  });
+});
+
+describe("ListDirectory Cache Function", () => {
+  const testObjectName = "testObject3";
+
+  let cacher = Cacher("./" + folderName);
+  cacher.save(testObjectName, testContentObject);
+
+  console.log(cacher.listDirectory(testObjectName));
+
+  test("List Directory cached objects", () => {
+    expect(
+      cacher.listDirectory(testObjectName).includes("testObject3.json")
+    ).toEqual(true);
+    fs.rmSync("./" + folderName + "/" + testObjectName + ".json");
+  });
 });
